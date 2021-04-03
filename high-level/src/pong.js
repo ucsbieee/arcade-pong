3f345c7b41ec9d22c0c9e60489b0ead8cd637e44
Sammy <sumezawa@protonmail.com>
2021-04-03 12:58:07 -0700

    start on Pong Pad and Ball classes

--- high-level/src/pong.js
+++ high-level/src/pong.js
@@ -2,6 +2,29 @@
 /* pong.js */
 
 
+// start button value and posedge
+var start_value = false;
+var start_pedge = false;
+
+// up button value and posedge
+var up_value = false;
+var up_pedge = false;
+
+// down button value and posedge
+var down_value = false;
+var down_pedge = false;
+
+
+const
+const bot_velocity = 10;
+
+class Pad {
+    constructor() {
+
+var pad_velocity = 0;
+var ball_velocity = 0;
+    }
+}
 function updatePPU() {
 
 }
