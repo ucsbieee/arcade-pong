3d69222c0bcd1f990066fbe03d8a33504e50d7c4
Ethan Sifferman <ethan@sifferman.dev>
2021-04-16 15:48:42 -0700

    es - added starter code for pong

--- high-level/src/pong.js
+++ high-level/src/pong.js
@@ -1,6 +1,33 @@
 
 /* pong.js */
 
+const   leftPaddle_x    = 0;
+var     leftPaddle_y    = 0;
+var     leftScore       = 0;
+
+const   rightPaddle_x   = 0;
+var     rightPaddle_y   = 0;
+var     rightScore      = 0;
+
+var     ball_xp         = 0;
+var     ball_xv         = 0;
+var     ball_yp         = 0;
+var     ball_yv         = 0;
+
+// fill OBM with paddle sprites
+function draw_paddles() {
+
+}
+// fill OBM with ball sprite
+function draw_ball() {
+
+}
+// fill NTBL with number_edge and number_corner tiles
+function draw_scores() {
+
+}
+
+
 
 function updatePPU() {
 
